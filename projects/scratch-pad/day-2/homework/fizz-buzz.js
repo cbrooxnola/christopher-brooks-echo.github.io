679e55d06b2439f1620d1360c24963e5e2824a1b
christopher brooks <csbroox@gmail.com>
2023-04-25 19:40:58 +0000

    fizzbuzz done

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,7 +12,18 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
+//print all numbers 1-100, except multiples of 3 and/or 5
+for (let i = 1; i < 101; i++){//for loop to contain numbers 1-100
+  if ((i % 3 === 0) && (i % 5 === 0)){
+    console.log("FizzBuzz");//log fizzbuzz if divisible by 3 and 5
+  } else if (i % 3 === 0){
+    console.log("Fizz"); //log fizz if divisible by 3 but not 5
+  } else if (i % 5 === 0){
+    console.log("Buzz");//log buzz if divisible by 5 but not 3
+  } else {
+    console.log(i);
+  }
+}
 
 
     // YOUR CODE GOES ABOVE HERE //
